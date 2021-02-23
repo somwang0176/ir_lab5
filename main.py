474809cc21ada87f25d5e6ff3f3cb8a1861ec7f8
somwang0176 <69186350+somwang0176@users.noreply.github.com>
2021-02-23 15:41:26 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, File, UploadFile
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/text/{text}")
+def read_word(text: str):
+    return {"word": text)}
